8af0f8377d9fc5c1efca157d963929391ce312fd
Chris Collier <czcollier@gmail.com>
2013-05-14 19:06:04 -0400

    fixes #63
    - Use Play classloader when deserializing objects with ObjectInputStream
      instead of default
    - Made RedisPlugin code more Scala-ish

--- redis/src/main/scala/com/typesafe/plugin/RedisPlugin.scala
+++ redis/src/main/scala/com/typesafe/plugin/RedisPlugin.scala
@@ -7,6 +7,8 @@ import play.api.cache._
 import java.util._
 import java.io._
 import biz.source_code.base64Coder._
+import akka.util.ClassLoaderObjectInputStream
+import scala.collection.immutable.MapLike
 
 /**
  * provides a redis client and a CachePlugin implementation
@@ -16,127 +18,142 @@ import biz.source_code.base64Coder._
  */
 class RedisPlugin(app: Application) extends CachePlugin {
 
- 
- private lazy val host = app.configuration.getString("redis.host").getOrElse("localhost")
- private lazy val port = app.configuration.getInt("redis.port").getOrElse(6379)
- private lazy val timeout = app.configuration.getInt("redis.timeout").getOrElse(2000)
- private lazy val password = app.configuration.getString("redis.password").getOrElse(null)
+  import play.api.Play.current
 
- /**
-  * provides access to the underlying jedis Pool
-  */
- lazy val jedisPool = new JedisPool(new JedisPoolConfig(), host, port, timeout, password)
+  private lazy val host = app.configuration.getString("redis.host").getOrElse("localhost")
+  private lazy val port = app.configuration.getInt("redis.port").getOrElse(6379)
+  private lazy val timeout = app.configuration.getInt("redis.timeout").getOrElse(2000)
+  private lazy val password = app.configuration.getString("redis.password").getOrElse(null)
 
   /**
-  * provides access to the sedis Pool
-  */           
- lazy val sedisPool = new Pool(jedisPool)
+   * provides access to the underlying jedis Pool
+   */
+  lazy val jedisPool = new JedisPool(new JedisPoolConfig(), host, port, timeout, password)
 
- override def onStart() {
+  /**
+   * provides access to the sedis Pool
+   */
+  lazy val sedisPool = new Pool(jedisPool)
+
+  override def onStart() {
     sedisPool
- }
+  }
 
- override def onStop() {
+  override def onStop() {
     jedisPool.destroy()
- }
+  }
 
- override lazy val enabled = {
+  override lazy val enabled = {
     !app.configuration.getString("redisplugin").filter(_ == "disabled").isDefined
   }
 
- /**
-  * cacheAPI implementation
-  * can serialize, deserialize to/from redis
-  * value needs be Serializable (a few primitive types are also supported: String, Int, Long, Boolean) 
-  */
- lazy val api = new CacheAPI {
+
+  type Flushable = Closeable { def flush() }
+
+  trait CanCleanup[-T] { def cleanup(x: T) }
+
+  implicit object CanCleanupCloseable extends CanCleanup[Closeable] {
+    def cleanup(cl: Closeable) { cl.close() }
+  }
+
+//  implicit object CanCleanupFlushable extends CanCleanup[Flushable] {
+//    def cleanup(fl: Flushable) { fl.close(); fl.flush() }
+//  }
+
+  def using[T, U](resource: T)(block: T => U)(implicit cleaner: CanCleanup[T]): U = {
+    try {
+      block(resource)
+    } finally {
+      if (resource != null) cleaner.cleanup(resource)
+    }
+  }
+
+  class PlayObjectInputStream(is: InputStream)(implicit app: play.Application) extends ClassLoaderObjectInputStream(Play.classloader, is)
+
+  /**
+   * cacheAPI implementation
+   * can serialize, deserialize to/from redis
+   * value needs be Serializable (a few primitive types are also supported: String, Int, Long, Boolean)
+   */
+  lazy val api = new CacheAPI {
 
     def set(key: String, value: Any, expiration: Int) {
-     var oos: ObjectOutputStream = null
-     var dos: DataOutputStream = null
-     try { 
-       val baos = new ByteArrayOutputStream()
-       var prefix = "oos"
-       if (value.getClass.isInstanceOf[Serializable]) {
-          oos = new ObjectOutputStream(baos)
-          oos.writeObject(value)
-          oos.flush()
-       } else if (value.getClass.isInstanceOf[String]) {
-          dos = new DataOutputStream(baos)
-          dos.writeUTF(value.asInstanceOf[String])
-          prefix = "string"
-       } else if (value.getClass.isInstanceOf[Int]) {
-          dos = new DataOutputStream(baos)
-          dos.writeInt(value.asInstanceOf[Int])
-          prefix = "int"
-       } else if (value.getClass.isInstanceOf[Long]) {
-          dos = new DataOutputStream(baos)
-          dos.writeLong(value.asInstanceOf[Long])
-          prefix = "long"
-       } else if (value.getClass.isInstanceOf[Boolean]) {
-          dos = new DataOutputStream(baos)
-          dos.writeBoolean(value.asInstanceOf[Boolean]) 
-          prefix = "boolean"   
-       } else {
-          throw new IOException("could not serialize: "+ value.toString)
-       }
-       val redisV = prefix + "-" + new String( Base64Coder.encode( baos.toByteArray() ) )  
-       Logger.warn(redisV)
-       sedisPool.withJedisClient { client =>
-          client.set(key,redisV)
-          if (expiration != 0) client.expire(key,expiration)
-       }
-     } catch {case ex: IOException =>
-       Logger.warn("could not serialize key:"+ key + " and value:"+ value.toString + " ex:"+ex.toString)
-     } finally {
-       if (oos != null) oos.close()
-       if (dos != null) dos.close()
-     }
-    
-    }
-    def remove(key: String): Unit =  sedisPool.withJedisClient { client => client.del(key) }
 
-    def get(key: String): Option[Any] = {
-      var ois: ObjectInputStream = null
-      var dis: DataInputStream = null
       try {
-        val data: Seq[String] =  sedisPool.withJedisClient { client =>
-            client.get(key)
-        }.split("-")
-        val b = Base64Coder.decode(data.last)
-        data.head match {
-          case "oos" => 
-              ois = new ObjectInputStream(new ByteArrayInputStream(b))
-              val r  = ois.readObject()
-              Some(r)
-          case "string" => 
-              dis = new DataInputStream(new ByteArrayInputStream(b))
-              val r  = dis.readUTF()
-              Some(r)
-          case "int" =>  
-              dis = new DataInputStream(new ByteArrayInputStream(b))
-               val r  = dis.readInt
-              Some(r)
-          case "long" => 
-              dis = new DataInputStream(new ByteArrayInputStream(b))
-              val r  = dis.readLong
-              Some(r)
-          case "boolean" =>   
-              dis = new DataInputStream(new ByteArrayInputStream(b))
-              val r  = dis.readBoolean
-              Some(r)
-          case _ => throw new IOException("can not recognize value")
+        val baos = new ByteArrayOutputStream()
+
+        val prefix = value match {
+          case ov: Serializable => using(new ObjectOutputStream(baos)) {
+            oos => oos.writeObject(ov); "oos"
+          }
+          case x => {
+            using(new DataOutputStream(baos)) {
+              dos =>
+                x match {
+                  case v: String => dos.writeUTF(v); "string"
+                  case v: Int => dos.writeInt(v); "int"
+                  case v: Long => dos.writeLong(v); "long"
+                  case v: Boolean => dos.writeBoolean(v); "boolean"
+                  case _ => throw new IOException("could not serialize: " + value.toString)
+                }
+            }
+          }
         }
-        
-      } catch {case ex: Exception => 
-        Logger.warn("could not deserialize key:"+ key+ " ex:"+ex.toString)
-        ex.printStackTrace()
-        None
-      } finally {
-       if (ois != null) ois.close()
-       if (dis != null) dis.close()
+        val redisV = prefix + "-" + String.valueOf(Base64Coder.encode(baos.toByteArray))
+
+        Logger.warn(redisV)
+
+        sedisPool.withJedisClient {
+          client =>
+            client.set(key, redisV)
+            if (expiration != 0) client.expire(key, expiration)
+        }
+      }
+      catch {
+        case ex: IOException =>
+          Logger.warn("could not serialize key:" + key + " and value:" + value.toString + " ex:" + ex.toString)
       }
     }
 
+    def remove(key: String) {
+      sedisPool.withJedisClient {
+        client => client.del(key)
+      }
+    }
+
+    def get(key: String): Option[Any] = {
+      try {
+        val data: Seq[String] = sedisPool.withJedisClient { _.get(key) }.split("-")
+
+        val bis = new ByteArrayInputStream(Base64Coder.decode(data.last))
+
+        val elem = data.head match {
+          case "oos" =>
+            using(new ClassLoaderObjectInputStream(Play.classloader, bis)) {
+              is => is.readObject
+            }
+          case x => {
+            using(new DataInputStream(bis)) {
+              is => x match {
+                  case "string" => is.readUTF
+                  case "int" => is.readInt
+                  case "long" => is.readLong
+                  case "boolean" => is.readBoolean
+                  case _ => throw new IOException("can not recognize value")
+                }
+            }
+          }
+        }
+
+        Some(elem)
+
+      }
+      catch {
+        case ex: Exception =>
+          Logger.warn("could not deserialisze key:" + key + " ex:" + ex.toString)
+          ex.printStackTrace()
+          None
+      }
+    }
   }
 }
